3097b6f42433a7b36bd8833e9248223dd6a53722
JoeJoePotato <34780073+JoeJoePotato@users.noreply.github.com>
2022-08-17 22:20:00 -0700

    ascii stuff

---
+++ Unit00/src/AsciiArt.java
@@ -0,0 +1,27 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Graham \n\n" );
+		System.out.println("Turtle" );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("     _________         " );
+		System.out.println("  /\\/ o o o o \\            " );
+		System.out.println("  \\ \\o o o o o \\         " );
+		System.out.println("   \\_\\_________/         " );
+		System.out.println("    || ||  || ||             " );
+		
+		//add other output
+
+		 
+	}
+}
---
+++ Unit00/src/AsciiBox.java
@@ -0,0 +1,21 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Graham \t  8/17/22 \n\n" );
+		for (int i = 0; i < "+++++++++++++++++++++++++ ".length(); i++) {
+			System.out.println("+++++++++++++++++++++++++ " );
+		}
+		
+		
+
+	}
+}
