84f4151280a2aa5559bd5cd768d98df5a961b42b
JoeJoePotato <34780073+JoeJoePotato@users.noreply.github.com>
2022-08-18 11:00:54 -0700

    Trap DONE

---
+++ Unit02/src/Trap.java
@@ -0,0 +1,14 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name - Graham Garibaldi
+//Date - 8/18/22
+//Class - APCSA period 2, 2022
+
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		double d=0.5*(base1+base2)*height;
+		return d;
+	}
+}
