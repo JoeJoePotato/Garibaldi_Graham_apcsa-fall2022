656c42e138fd58709cbc8e127dee7d1f45e2f916
JoeJoePotato <34780073+JoeJoePotato@users.noreply.github.com>
2022-08-19 10:26:26 -0700

    cube done, unit 02 complete

---
+++ Unit02/src/Cube.java
@@ -0,0 +1,13 @@
+//Name - Graham Garibaldi
+//Date - 8/19/22
+//Class - APCSA period 2, 2022
+
+public class Cube {
+	public static double area( int side )
+	{
+		
+		double a=side*side*6;
+
+		return a;
+	}
+}
---
+++ Unit02/src/CubeRunner.java
@@ -0,0 +1,20 @@
+//Name - Graham Garibaldi
+//Date - 8/19/22
+//Class - APCSA period 2, 2022
+
+public class CubeRunner
+{
+	public static void main( String[] args )
+   {
+		System.out.println( "Cube surface area is :: " + Cube.area( 112 ) );
+		System.out.println( "Cube surface area is :: " + Cube.area( 4 ) );
+		System.out.println( "Cube surface area is :: " + Cube.area( 33 ) );
+		System.out.println( "Cube surface area iss :: " + Cube.area( 50 ) );
+		System.out.println( "Cube surface area is :: " + Cube.area( 5 ) );		
+		System.out.println( "Cube surface area is :: " + Cube.area( 19 ) );
+		System.out.println( "Cube surface area is :: " + Cube.area( 111 ) );
+
+	}
+}
+
+
