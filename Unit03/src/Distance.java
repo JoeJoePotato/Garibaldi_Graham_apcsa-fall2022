810265e5246ea335a25cb477c4e2f942993b0efb
JoeJoePotato <34780073+JoeJoePotato@users.noreply.github.com>
2022-08-19 11:23:17 -0700

    Distance done, Unit 03 done

--- Unit03/src/Distance.java
+++ Unit03/src/Distance.java
@@ -13,29 +13,33 @@ public class Distance
 	private int xOne,yOne,xTwo,yTwo;
 	private double distance;
 
-	public Distance()
-	{
-	}
-
 	public Distance(int x1, int y1, int x2, int y2)
 	{
+	 this.xOne=x1;
+	 this.yOne=y1;
+	 this.xTwo=x2;
+	 this.yTwo=y2;
 	}
 
-	public void setCoordinates(int x1, int y1, int x2, int y2)
-	{
-	}
 
 	public void calcDistance()
 	{
+	int xDistance=(xTwo-xOne)*(xTwo-xOne);
+	int yDistance=(yTwo-yOne)*(yTwo-yOne);	
+	double pythagorasstolealotofhisproofs=xDistance+yDistance;
+	distance=Math.sqrt(pythagorasstolealotofhisproofs);
+	
 	}
 	
 	public double getDistance()
 	{
-		return 0.0;
+		calcDistance();
+		return distance;
 	}
 	
 	public void print()
 	{
+		System.out.printf("%.3f\n", getDistance());
 	}
 	
 	//complete print or the toString
--- Unit03/src/DistanceRunner.java
+++ Unit03/src/DistanceRunner.java
@@ -12,7 +12,30 @@ public class DistanceRunner
 {
 	public static void main( String[] args )
 	{
-		//add test cases	
-			
+		testDistance();
+		testDistance();
+		testDistance();
 	}
+
+	public static void testDistance() {
+		Scanner keyboard = new Scanner(in);
+
+		out.print("Enter x1 :: ");
+		int x1 = keyboard.nextInt();
+
+		out.print("Enter y1 :: ");
+		int y1 = keyboard.nextInt();
+
+		out.print("Enter x2 :: ");
+		int x2 = keyboard.nextInt();
+
+		out.print("Enter y2 :: ");
+		int y2 = keyboard.nextInt();
+		
+		Distance test = new Distance(x1, y1, x2, y2);
+		test.print();
+	}
+	
+
+
 }
