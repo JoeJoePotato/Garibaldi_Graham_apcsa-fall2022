16d38832575250b9fa60350c1be3944945e02860
JoeJoePotato <34780073+JoeJoePotato@users.noreply.github.com>
2022-08-22 11:40:16 -0700

    set up unit 5

---
+++ Unit05/src/WordsCompare.java
@@ -0,0 +1,58 @@
+//Name - Graham Garibaldi
+//Date - 8/22/22
+//Class - APCSA period 2, 2022
+
+import static java.lang.System.*;
+
+public class WordsCompare
+{
+	private String wordOne, wordTwo;
+   private boolean compare;
+
+	public WordsCompare()
+	{
+		
+	}
+
+	public WordsCompare(String one, String two)
+	{
+		this.wordOne=one;
+		this.wordTwo=two;
+	}
+
+	public void setWords(String one, String two)
+	{
+		wordOne=one;
+		wordTwo=two;
+	}
+
+	public boolean compare()
+	{
+		char firstOne=wordOne.charAt(0);
+		char firstTwo=wordTwo.charAt(0);
+		if(firstOne<firstTwo) {
+			return true;
+		}
+		if(firstOne>firstTwo) {
+			return false;
+		}
+		if(wordOne.length()<wordTwo.length()) {
+			return true;
+		}
+		
+	return false;
+	}
+
+	public String toString()
+	{
+		compare=compare();
+		if(compare==true)
+		{
+			return wordOne + " should be placed before " + wordTwo + "\n";
+		}
+		else
+		{
+			return wordOne + " should be placed after " + wordTwo + "\n";
+		}
+	}
+}
---
+++ Unit05/src/WordsCompareRunner.java
@@ -0,0 +1,25 @@
+//Name - Graham Garibaldi
+//Date - 8/22/22
+//Class - APCSA period 2, 2022
+
+import static java.lang.System.*;
+
+public class WordsCompareRunner
+{
+   public static void main( String args[] )
+   {
+		/*
+abe ape
+giraffe gorilla
+one two
+fun funny
+123 19
+193 1910
+goofy godfather
+funnel fun
+
+		 */
+
+	   
+	}
+}
