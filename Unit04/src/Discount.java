79a559a27acfade0bb1e9676d09af811877835ba
JoeJoePotato <34780073+JoeJoePotato@users.noreply.github.com>
2022-08-22 11:00:02 -0700

    discount done

--- Unit04/src/Discount.java
+++ Unit04/src/Discount.java
@@ -1,4 +1,21 @@
+//Name - Graham Garibaldi
+//Date - 8/19/22
+//Class - APCSA period 2, 2022
 
-public class Discount {
+import static java.lang.System.*;
+import java.util.Scanner;
 
+public class Discount
+{
+	//instance variables and constructors could be used, but are not really needed
+	
+	//getDiscountedBill() will return final amount of the bill
+	//			if the bill is >2000, the bill receives a 15% discount
+	public static double getDiscountedBill(double bill)
+	{
+		if(bill>2000) {
+			bill=bill*0.85;
+		}
+		return bill;
+	}
 }
--- Unit04/src/DiscountRunner.java
+++ Unit04/src/DiscountRunner.java
@@ -1,4 +1,24 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
 
-public class DiscountRunner {
+//Name - 
+//Date -
+//Class -
+//Lab  -
 
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class DiscountRunner
+{
+	public static void main( String args[] )
+	{
+		Discount d=new Discount();
+		Scanner keyboard = new Scanner(System.in);
+		
+		out.print("Enter the original bill amount :: ");
+		double amt = keyboard.nextDouble();
+		System.out.printf("%.2f\n", d.getDiscountedBill(amt));
+
+	}
 }
