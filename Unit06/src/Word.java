43bb0eb8c874aef137b2f91736bde87ff6cf3c43
JoeJoePotato <34780073+JoeJoePotato@users.noreply.github.com>
2022-08-26 10:36:52 -0700

    wordDone

--- Unit06/src/Word.java
+++ Unit06/src/Word.java
@@ -1,4 +1,44 @@
+import static java.lang.System.*;
 
-public class Word {
+public class Word
+{
+	private String word;
 
+	public Word()
+	{
+	}
+
+	public Word(String s)
+	{
+		word=s;
+	}
+
+	public void setString(String s)
+	{
+		word=s;
+	}
+
+	public char getFirstChar()
+	{
+		return word.charAt(0);
+	}
+
+	public char getLastChar()
+	{
+		return word.charAt(word.length()-1);
+	}
+
+	public String getBackWards()
+	{
+		String back="";
+		for (int i = word.length()-1; i >=0; i--) {
+			back+=word.charAt(i);
+		}
+		return back;
+	}
+
+ 	public String toString()
+ 	{
+ 		return getFirstChar()+"\n"+getLastChar()+"\n"+getBackWards()+"\n"+word;
+	}
 }
--- Unit06/src/WordRunner.java
+++ Unit06/src/WordRunner.java
@@ -1,4 +1,19 @@
+import static java.lang.System.*;
 
-public class WordRunner {
-
+public class WordRunner
+{
+	public static void main ( String[] args )
+	{
+		Word w=new Word("Hello");
+		System.out.println(w);
+		w.setString("World");
+		System.out.println(w);
+		w.setString("JukeBox");
+		System.out.println(w);
+		w.setString("TCEA");
+		System.out.println(w);
+		w.setString("UIL");
+		System.out.println(w);
+		
+	}
 }
