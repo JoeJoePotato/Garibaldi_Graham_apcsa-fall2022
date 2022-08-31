89e3200be1f7390932de11bf882b54424ec68b75
JoeJoePotato <34780073+JoeJoePotato@users.noreply.github.com>
2022-08-31 12:09:03 -0700

    RockPaperScissors

--- Unit07/src/RPSRunner.java
+++ Unit07/src/RPSRunner.java
@@ -10,15 +10,35 @@ public class RPSRunner
 		char response;
 		
 		//add in a do while loop after you get the basics up and running
-		
-			String player = "";
+		int playerscore=0;
+		int computerscore=0;
+		int a=1;
+		while(a==1) {
 		
 			out.print("type in your prompt [R,P,S] :: ");
 			String prompt=keyboard.next();
 			//read in the player value
 			
 			RockPaperScissors game = new RockPaperScissors(prompt);		
-	}
+			String s=game.toString();
+			System.out.println(s);
+			
+			if(s.startsWith("player")) {
+				playerscore++;
+			}else if(s.startsWith("computer")) {
+				computerscore++;
+			}
+			
+			System.out.println("Player-"+playerscore+"vs. Computer-"+computerscore);
+			if(computerscore==2) {
+				System.out.println("COMPUTER WINS");
+				a=2;
+			}else if(playerscore==2) {
+				System.out.println("PLAYER WINS");
+				a=2;
+			}
+		}
+		}
 }
 
 
--- Unit07/src/RockPaperScissors.java
+++ Unit07/src/RockPaperScissors.java
@@ -1,13 +1,16 @@
+import java.util.Random;
 import java.util.Scanner;
 import static java.lang.System.*;
 
 import java.util.Scanner;
 import static java.lang.System.*;
+import java.util.random.*;
 
 public class RockPaperScissors
 {
 	private String playChoice;
 	private String compChoice;
+	Random r;
 
 	public RockPaperScissors()
 	{
@@ -15,21 +18,50 @@ public class RockPaperScissors
 
 	public RockPaperScissors(String player)
 	{
+		playChoice=player;
+		r=new Random();
 	}
 
 	public void setPlayers(String player)
 	{
+		playChoice=player;
 	}
 
 	public String determineWinner()
 	{
+		//0=Rock
+		//1=Paper
+		//2=Scissors
 		String winner="";
+		int compplay=r.nextInt(3);
+	
+		if(compplay==0 && playChoice.equals("S")) {
+			winner="computer wins! Rock crushes scissors";
+		}else if(compplay==1 && playChoice.equals("R")) {
+			winner="computer wins! Paper covers rock!";
+		}else if(compplay==2 && playChoice.equals("P")) {
+			winner="computer wins! Scissors cut paper";
+		}else if(compplay==0 && playChoice.equals("P")) {
+			winner="player wins! Paper covers rock";
+		}else if(compplay==1 && playChoice.equals("S")) {
+			winner="player wins! Scissors cut paper";
+		}else if(compplay==2 && playChoice.equals("R")) {
+			winner="player wins! Rock crushes scissors";
+		}
+
 		return winner;
 	}
 
 	public String toString()
 	{
-		String output="";
+		String s=determineWinner();
+		String output=s;
+		if(output.equals("")) {
+			output="Draw game!";
+		}
+		
+
+		
 		return output;
 	}
 }
